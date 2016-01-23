c6e5bda93b1025fdd1bef926e69af37fe87e09b9
Bryce Mecum <petridish@gmail.com>
2016-01-22 16:03:24 -0900

    Add routines for setting up the file inventory

---
+++ R/inventory.R
@@ -0,0 +1,166 @@
+#' inventory.R
+#' Author: Bryce Mecum <mecum@nceas.ucsb.edu>
+#'
+#' Functions relating to keeping up an inventory of files that exist on the KNB
+#' and may or may not be copied to another computer and untarred.
+#'
+
+library(dplyr)
+
+
+#' Create an empty inventory data.frame. This doesn't need to be a function
+#' but I'm making it one in case the initialization routine becomes more
+#' complicated.
+#'
+#' @return An empty data frame
+#' @export
+#'
+#' @examples
+#' inv_init()
+inv_init <- function() {
+  inventory <- data.frame(stringsAsFactors = FALSE)
+
+  inventory
+}
+
+#' Load filenames into the inventory from a text file.
+#'
+#' Filenames should be the output of the command:
+#'
+#'   you@server:/home/jones/acadis$ find . -type f
+#'
+#' @param filename Filepath to a file containing filenames.
+#' @param inventory A \code{data.frame}.
+#'
+#' @return An inventory (data.frame)
+#'
+#' @examples
+#' inv_load_filenames("some/file/path.txt", my_inventory)
+inv_load_filenames <- function(filename, inventory) {
+  stopifnot(file.exists(filename))
+  stopifnot("inventory" %in% ls(),
+            is.data.frame(inventory))
+
+  # Read the filenames from disk
+  filenames <- read.table(filename, col.names = c("filename"), stringsAsFactors = FALSE)
+
+  stopifnot(is.data.frame(filenames))
+
+  # If inventory is empty, just make the inventory the same as filenames
+  if (nrow(inventory) == 0) {
+    return(filenames)
+  }
+
+  # Only append rows with new filenames
+  #
+  # Merging algorithm:
+  #   Add colums we need to `filenames`
+  #   Remove rows from `filenames` that exist in `inventory`
+  #   Merge the two
+
+  # Make `filenames` the same shape as `inventory` by appending columns
+  for (col_name in names(inventory)) {
+    if (col_name %in% names(filenames)) { next }
+    filenames[,col_name] <- NA
+  }
+
+  stopifnot(identical(names(inventory), names(filenames)))
+
+  inter_vals <- intersect(inventory$filename, filenames$filename)
+
+  # Remove intersections if we had any
+  if (length(inter_vals) > 0 ) {
+    filenames <- subset(filenames, match(filename, inter_vals, nomatch = 0) == 0)
+  }
+
+  inventory <- rbind(inventory,
+                     filenames)
+
+  inventory
+}
+
+#' Load file sizes into an inventory from a text file. Removes the column
+#' 'size_bytes' from inventory before doing a left join.
+#'
+#' @param filename Filepath to a file containing sizes.
+#' @param inventory A \code{data.frame}.
+#'
+#' @return An inventory (data.frame)
+#'
+#' @examples
+inv_load_sizes <- function(filename, inventory) {
+  stopifnot(file.exists(filename))
+  stopifnot("inventory" %in% ls(),
+            is.data.frame(inventory),
+            "filename" %in% names(inventory))
+
+  # Read the sizes from disk
+  sizes <- read.table(filename, col.names = c("size_bytes", "filename"), stringsAsFactors = FALSE)
+
+  stopifnot(is.data.frame(sizes))
+
+  # Join the sizes onto existing filenames in the inventory
+  # First drop the existing checksums
+  inventory <- inventory[,!(names(inventory) %in% "size_bytes"), drop=FALSE]
+  inventory <- left_join(inventory, sizes, by="filename")
+
+  inventory
+}
+
+#' Load checksums into the inventory file from a text file. This function
+#' removes the column 'checksum_sha256' from inventory before doing a
+#' left join.
+#'
+#' @param filename Filepath to a file containing sizes.
+#' @param inventory A \code{data.frame}.
+#'
+#' @return An inventory (data.frame)
+#'
+#' @examples
+inv_load_checksums <- function(filename, inventory) {
+  stopifnot(file.exists(filename))
+  stopifnot("inventory" %in% ls(),
+            is.data.frame(inventory),
+            "filename" %in% names(inventory))
+
+  # Read the sizes from disk
+  checksums <- read.table(filename, col.names = c("checksum_sha256", "filename"), stringsAsFactors = FALSE)
+
+  stopifnot(is.data.frame(checksums))
+
+  # Join the checksums onto existing filenames in the inventory
+  # First drop the existing checksums
+  inventory <- inventory[,!(names(inventory) %in% "checksum_sha256"), drop=FALSE]
+  inventory <- left_join(inventory, checksums, by="filename")
+
+  inventory
+}
+
+#' Load identifiers into the inventory file from a text file. This function
+#' removes the column 'identifier' from inventory before doing a
+#' left join.
+#'
+#' @param filename Filepath to a file containing identifiers.
+#' @param inventory A \code{data.frame}.
+#'
+#' @return An inventory (data.frame)
+#'
+#' @examples
+inv_load_identifiers <- function(filename, inventory) {
+  stopifnot(file.exists(filename))
+  stopifnot("inventory" %in% ls(),
+            is.data.frame(inventory),
+            "filename" %in% names(inventory))
+
+  # Read the identifiers from disk
+  identifiers <- read.table(filename, col.names = c("identifier", "filename"), stringsAsFactors = FALSE)
+
+  stopifnot(is.data.frame(identifiers))
+
+  # Join the identifiers onto existing filenames in the inventory
+  # First drop the existing identifiers
+  inventory <- inventory[,!(names(inventory) %in% "identifier"), drop=FALSE]
+  inventory <- left_join(inventory, identifiers, by="filename")
+
+  inventory
+}
---
+++ tests/testthat/test_inventory.R
@@ -0,0 +1,71 @@
+#' test_inventory.R
+
+test_that("an inventory can be created correctly", {
+  x <- inv_init()
+  expect_true(is.data.frame(x))
+  expect_true(nrow(x) == 0)
+})
+
+test_that("an inventory can be populated with filenames", {
+  # Case 1: Empty inv, non-empty file
+  inv <- inv_init()
+  result <- inv_load_filenames(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "filenames_simple.csv"), inv)
+  expect_true(nrow(result) == 3)
+
+  # Case 2: Non-empty inv, non-empty file, where some are the same
+  inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
+  result <- inv_load_filenames(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "filenames_simple.csv"), inv)
+  expect_true(nrow(result) == 5)
+
+  # Case 3: Non-empty inv, non-empty file, where none are the same
+  inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
+  inv <- subset(inv, filename != "C")
+  result <- inv_load_filenames(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "filenames_simple.csv"), inv)
+  expect_true(nrow(result) == 5)
+
+  # Case 4: Non-empty inv, non-empty file, inventory already has columns
+  inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
+  inv$bytes <- rep(1000, nrow(inv))
+  result <- inv_load_filenames(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "filenames_simple.csv"), inv)
+
+  expect_true(nrow(result) == 5)
+  expect_true(ncol(result) == 2)
+  expect_true(length(table(result$bytes, exclude=NULL)) == 2)
+})
+
+test_that("an inventory can be populated with byte sizes", {
+  inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
+  result <- inv_load_sizes(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "sizes_simple.txt"), inv)
+
+  expect_true(nrow(result) == 3)
+  expect_true(ncol(result) == 2)
+})
+
+test_that("an inventory can be populated with checksums", {
+  # Test if we can populate with checksums before sizes
+  inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
+  inv_with_checksums <- inv_load_checksums(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"), inv)
+  inv_with_both <- inv_load_sizes(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "sizes_simple.txt"), inv_with_checksums)
+
+  expect_true(nrow(inv_with_both) == 3)
+  expect_true(ncol(inv_with_both) == 3)
+
+  # Test if we can populate with sizes before checksums
+  inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
+  inv_with_sizes <- inv_load_sizes(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "sizes_simple.txt"), inv)
+  inv_with_both <- inv_load_checksums(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"), inv_with_sizes)
+
+  expect_true(nrow(inv_with_both) == 3)
+  expect_true(ncol(inv_with_both) == 3)
+})
+
+# We should be able to call the same function multiple times and not break things
+test_that("calling things repeatedly does not break things", {
+  inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
+  inv <- inv_load_checksums(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"), inv)
+  inv$test <- NA
+  inv <- inv_load_checksums(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"), inv)
+
+  expect_true(ncol(inv) == 3)
+  expect_true(nrow(inv) == 3)
+})
