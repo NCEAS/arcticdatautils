56914da8ba1faf1f71bb2f16c5d733618a1ef55c
Bryce Mecum <petridish@gmail.com>
2016-01-27 16:07:44 -0900

    Fill in stub of resource map function

--- R/packaging.R
+++ R/packaging.R
@@ -4,17 +4,181 @@
 #' Code related to packaging datasets.
 
 library(whisker)
+library(redland)
 
 #' Create a Resource Map XML string suitable for use in an MN.Create() call
 #'
-#' @param variables
+#' Relevant documentation:
+#' https://jenkins-ucsb-1.dataone.org/job/API%20Documentation%20-%20trunk/ws/api-documentation/build/html//design/DataPackage.html?highlight=resourcemap#generating-resource-maps
 #'
-#' @return
+#' @param scimeta_objects
+#' @param data_objects
+#' @param child_resource_map
+#'
+#' @return The text of the file (character)
 #' @export
 #'
 #' @examples
-create_resource_map <- function(variables) {
-  "some_xml"
+create_resource_map <- function(scimeta_objects, data_objects, child_resource_map) {
+  # Create objects related to an RDF Model
+  world <- new("World")
+  storage <- new("Storage", world, "hashes", name="", options="hash-type='memory'")
+  model <- new("Model", world=world, storage, options="")
+
+  # Set up the namespaces we need
+  rdf <- "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  rdfs1 <- "http://www.w3.org/2001/01/rdf-schema#"
+  dc <- "http://purl.org/dc/elements/1.1/"
+  dcterms <- "http://purl.org/dc/terms/"
+  cito <- "http://purl.org/spar/cito/"
+  ore <- "http://www.openarchives.org/ore/terms/"
+
+  # Temporary: Prepare the (invalid) URIs we'll need
+  resource_map_identifier <- "resmapid"
+  science_metadata_identifier <- "scimetaid"
+  dataobject_identifiers <- c("dataobj1id", "dataobj2id", "dataobj3id", "dataobj4id")
+  aggregation <- paste0(resource_map_identifier, "#aggregation")
+
+  # Add statemnts
+
+  # scimeta isDocumentedBy resourceMap
+  addStatement(model, new("Statement", world = world,
+                          subject=science_metadata_identifier,
+                          predicate=paste0(cito, "isDocumentedBy"),
+                          object=resource_map_identifier))
+
+  # aggregation type Aggregation
+  addStatement(model, new("Statement", world = world,
+                          subject=aggregation,
+                          predicate=paste0(rdf, "type"),
+                          object=paste0(ore, "Aggregation")))
+
+  # aggregation aggregates scimeta
+  addStatement(model, new("Statement", world = world,
+                          subject=aggregation,
+                          predicate=paste0(rdf, "aggregates"),
+                          object=science_metadata_identifier))
+
+  # aggregation aggregates dataobject
+  for (identifier in dataobject_identifiers) {
+    addStatement(model, new("Statement", world = world,
+                            subject=aggregation,
+                            predicate=paste0(rdf, "aggregates"),
+                            object=identifier))
+  }
+
+  # Aggregation label "Aggregation"
+  addStatement(model, new("Statement", world = world,
+                          subject=paste0(ore, "Aggregation"),
+                          predicate=paste0(rdfs1, "label"),
+                          object="Aggregation"))
+
+  # Aggregation isDefinedBy ORE
+  addStatement(model,  new("Statement", world = world,
+                           subject=paste0(ore, "Aggregation"),
+                           predicate=paste0(rdfs1, "isDefinedBy"),
+                           object=ore))
+
+  # ResourceMap label
+  addStatement(model, new("Statement", world = world,
+                          subject=paste0(ore, "ResourceMap"),
+                          predicate=paste0(rdfs1, "label"),
+                          object="ResourceMap"))
+
+  # ResourceMap isDefinedBy ORE
+  addStatement(model, new("Statement", world = world,
+                          subject=paste0(ore, "ResourceMap"),
+                          predicate=paste0(rdfs1, "isDefinedBy"),
+                          object=ore))
+
+  # dataobject isDocumentedBy + identifier
+  for (identifier in dataobject_identifiers) {
+    addStatement(model, new("Statement", world = world,
+                            subject=identifier,
+                            predicate=paste0(cito, "isDocumentedBy"),
+                            object=science_metadata_identifier))
+
+    addStatement(model, new("Statement", world = world,
+                            subject=identifier,
+                            predicate=paste0(dcterms, "identifier"),
+                            object=identifier))
+  }
+
+  # scimeta identifier
+  addStatement(model, new("Statement", world = world,
+                          subject=science_metadata_identifier,
+                          predicate=paste0(dcterms, "identifier"),
+                          object=science_metadata_identifier))
+
+  # scimeta documents dataobject
+  for (identifier in dataobject_identifiers) {
+    addStatement(model, new("Statement", world = world,
+                            subject=science_metadata_identifier,
+                            predicate=paste0(cito, "documents"),
+                            object=identifier))
+  }
+
+  # resource_map type ResourceMap
+  addStatement(model, new("Statement", world = world,
+                          subject=resource_map_identifier,
+                          predicate=paste0(rdf, "type"),
+                          object=paste0(ore, "ResourceMap")))
+
+  # resource_map identifier
+  addStatement(model, new("Statement", world = world,
+                          subject=resource_map_identifier,
+                          predicate=paste0(dcterms, "identifier"),
+                          object=resource_map_identifier))
+
+  # resource_map format
+  addStatement(model, new("Statement", world = world,
+                          subject=resource_map_identifier,
+                          predicate=paste0(dc, "format"),
+                          object="application/rdf+xml"))
+
+  # resource_map describes aggregation
+  addStatement(model, new("Statement", world = world,
+                          subject=resource_map_identifier,
+                          predicate=paste0(ore, "describes"),
+                          object=aggregation))
+
+  # resource_map creator
+  addStatement(model, new("Statement", world = world,
+                          subject=resource_map_identifier,
+                          predicate=paste0(dcterms, "creator"),
+                          object="The `arcticdata` R package."))
+
+  now <- format(Sys.time(), "%Y-%m-%dT%H:%M:%S.000%z")
+
+  # resource_map created
+  addStatement(model, new("Statement", world = world,
+                          subject=resource_map_identifier,
+                          predicate=paste0(dcterms, "created"),
+                          object=now))
+
+  # resource_map modified
+  addStatement(model, new("Statement", world = world,
+                          subject=resource_map_identifier,
+                          predicate=paste0(dcterms, "modified"),
+                          object=now))
+
+  serializer <- new("Serializer", world, mimeType="application/rdf+xml")
+
+  # Set up serializer namespaces
+  setNameSpace(serializer, world, namespace=rdf, prefix="rdf")
+  setNameSpace(serializer, world, namespace=rdfs1, prefix="rdfs1")
+  setNameSpace(serializer, world, namespace=dc, prefix="dc")
+  setNameSpace(serializer, world, namespace=dcterms, prefix="dcterms")
+  setNameSpace(serializer, world, namespace=cito, prefix="cito")
+  setNameSpace(serializer, world, namespace=ore, prefix="ore")
+
+  serialized_resource_map <- serializeToCharacter(serializer, world, model)
+
+  freeSerializer(serializer)
+  freeModel(model)
+  freeWorld(world)
+
+  serialized_resource_map
 }
 
 
@@ -71,5 +235,3 @@ generate_system_metadata <- function(identifier,
 
   text
 }
-
-
