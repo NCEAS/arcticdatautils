7ef86d32c67879ad00a16fbf770dde6c263d13f4
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-02-18 16:41:09 -0900

    Completely overhaul insert_package and related funcs
    Most of what I'm doing involves uncertainty about what happens when we
    ask for PIDs, or ask to create(). This re-write cleans up
    insert_package() so it's shorter and calls out to functions that handle
    the various pieces of inserting a package. Uncertain function calls
    like create() are wrapped in tryCatch statements and errors are (in
    theory) handled.
    
    This rewrite also changes what is passed in and out of functions.
    insert_package() still takes an inventory and a package identifier, but
    now returns a (potentially) modified subset of the inventory -- the
    subset related to that package. What I mean by potentially is that, if
    a PID is created for an object but the object itself fails to create,
    we modify the subset of the inventory to save the PID so we don't have
    to mint the PID the next time. More on this in future documentation.

--- R/packaging.R
+++ R/packaging.R
@@ -1,155 +1,215 @@
 #' package.R
 #' Author: Bryce Mecum <mecum@nceas.ucsb.edu>
 #'
-#' Code related to packaging datasets.
+#' Code related to inserting datasets as Data Packages.
 
 
-#' Manually create a Data Package from files in the Inventory.
+#' Create a single package Data Package from files in the Inventory.
 #'
 #' @param inventory An Inventory (data.frame)
 #' @param package The package identifier (character)
 #' @param child_pids Resource Map PIDs for child Data Packages (character)
+#' @param env Environment variables (list)
 #'
-#' @return The PID of the Resource Map for the Data Package.
+#' @return A list containing PIDs and whether objects were inserted. (list)
 #' @export
 #'
 #' @examples
 #' insert_package(my_inventory_df, "my_package_id")
-insert_package <- function(inventory, package, child_pids=c()) {
+#'
+#' TODO
+#' Add routine to check for the files elsewhere
+#' This should be automated and based upon the inventory file having another
+#' column with that information
 
-  # TODO
-  # Add routine to check for the files elsewhere
-  # This should be automated and based upon the inventory file having another
-  # column with that information
+insert_package <- function(inventory, package, child_pids=c(), env=list()) {
+  # Validate the environment parameter
+  stopifnot(!is.null(env), length(env) > 0)
+  stopifnot(length(setdiff(c("base_directory", "identifier_scheme", "mn", "submitter", "rights_holder"), names(env))) == 0)
+  stopifnot(all(unlist(lapply(env, nchar)) > 0))
 
+  # Configuration
+  library(dataone)  # TODO Remove this library call once the package is fixed
+  mn <- MNode(env$mn)
+  submitter <- env$submitter
+  rights_holder <- env$rights_holder
+  base_path <- env$base_directory
 
+  stopifnot(class(mn) == "MNode",
+            nchar(submitter) > 0,
+            nchar(rights_holder) > 0,
+            file.exists(base_path))
 
-
-
-
-  # Debug
-  # inventory <- inv
-  # package <- "145b03155507c7c79a87ae95ecef170baef09148"
-  # child_packages <- c()
-
-  # Config
-  mn <- dataone::MNode("https://dev.nceas.ucsb.edu/knb/d1/mn/v2")
-  me <- "CN=Bryce Mecum A27576,O=Google,C=US,DC=cilogon,DC=org"
-  rh <- "CN=arctica,UID=arctica,DC=arctica,DC=org"
-  files_base_path <- "~/src/arctic-data/packages/"
 
   # Find the package contents (metadata and data)
   files <- inventory[inventory$package == package,]
-  # Remove this once I fix my bug
-  files <- files[!is.na(files$package),]
+  files <- files[!is.na(files$package),]   # TODO: Remove this once I fix my bug
   stopifnot(nrow(files) > 0)
 
-  # SCIENCE METADATA (1)
-  # Find the metadata object
-  files_metadata <- files[files$is_metadata == TRUE,]
-  stopifnot(nrow(files_metadata) == 1)
-  files_metadata[,"file"]
+  # Seperate files out into metadata and data by capturing their indicies
+  # in the files data.frame
+  files_idx_metadata <- which(files$is_metadata == TRUE)
+  files_idx_data <- which(files$is_metadata == FALSE)
+  stopifnot(length(files_idx_metadata) == 1)
 
 
-  path_on_disk <- paste0(files_base_path, files_metadata[1,"filename"])
+  # Process metadata
 
-  # Generate and save PID
-  metadata_pid <- dataone::generateIdentifier(mn)
-  metadata_sysmeta <- create_sysmeta(metadata_pid, files_metadata[1,], me, rh)
+  # Metadata PID
+  files[files_idx_metadata,"pid"] <- get_or_create_pid(files[files_idx_metadata,], mn, scheme="UUID")
 
-  cat(paste0("Caling MN.create() on ", files_metadata[1,"file"], " for PID '", metadata_pid, "'.\n"))
-  dataone::create(mn,
-                  metadata_pid,
-                  file = path_on_disk,
-                  sysmeta = metadata_sysmeta)
-
-
-  # DATA (MANY)
-  files_data <- files[files$is_metadata == FALSE,]
-  files_data[,"file"]
-
-  # Create them
-  # Mint PIDs, saving them for later
-  data_pids <- c()
-
-  for (i in seq_len(nrow(files_data))) {
-    path_on_disk <- paste0(files_base_path, files_data[i,"filename"])
-
-    # Generate and save PID
-    data_pid <- dataone::generateIdentifier(mn)
-
-    data_sysmeta <- create_sysmeta(data_pid, files_data[i,], me, rh)
-    cat(paste0("Caling MN.create() on ", files_data[i,"file"], " for PID '", data_pid, "'.\n"))
-    dataone::create(mn,
-                    data_pid,
-                    file = path_on_disk,
-                    sysmeta = data_sysmeta)
-
-    data_pids <- c(data_pids, data_pid)
+  if (is.na(files[files_idx_metadata,"pid"])) {
+    cat(paste0("Metadata PID was NA for package ", package, ".\n"))
+    return(files)
   }
 
-  # Generate a resource map
-  # Create the resource map
-  # Create sysmeta for the resource map
-  resource_map_filepath <- create_resource_map(metadata_pid, data_pids, child_pids)
-  resource_map_size_bytes <- file.info(resource_map_filepath)$size
-  resource_map_checksum <- digest::digest(resource_map_filepath, algo = "sha256")
-  resource_map_pid <- paste0("resourceMap_", metadata_pid)
+  # Metadata SystemMetadata
+  metadata_sysmeta <- create_sysmeta(files[files_idx_metadata,],
+                                     base_path,
+                                     submitter,
+                                     rights_holder)
 
-  resource_map_values <- data.frame("format_id" = "http://www.openarchives.org/ore/terms",
-                                    "size_bytes" = resource_map_size_bytes,
-                                    "checksum_sha256" = resource_map_checksum,
-                                    "file" = paste0(resource_map_pid, ".xml"),
-                                    stringsAsFactors = FALSE)
+  if (is.null(metadata_sysmeta)) {
+    cat(paste0("System Metadata creation failed for metadata object in package ", package, ".\n"))
+    return(files)
+  }
 
-  resource_map_sysmeta <- create_sysmeta(resource_map_pid,
-                                         resource_map_values[1,],
-                                         me,
-                                         rh)
+  # Metadata Object
+  files[files_idx_metadata,"created"] <- create_object(files[files_idx_metadata,],
+                                            metadata_sysmeta,
+                                            base_path,
+                                            mn)
 
-  response <- dataone::create(mn,
-                              resource_map_pid,
-                              file = resource_map_filepath,
-                              sysmeta = resource_map_sysmeta)
 
-  # TODO Log response
 
-  # Parse the response
-  stopifnot(!is.null(response))
 
-  resource_map_pid
+    if (files[files_idx_metadata,"created"] == FALSE) {
+      cat(paste0("Object creation failed for metadata object in package ", package, ".\n"))
+      return(files)
+    }
+
+  print(files)
+
+  return(files)
+
 }
 
-#' Create a SystemMetadata object. This is a convenience wrapper around just
-#' calling new("SystemMetadata", ...) directly.
-#'
-#' @param pid The PID for the Object. (character)
-#' @param inventory_file An Inventory
-#' @param submitter The DN for the submitter field (character)
-#' @param rights_holder The DN for the rightsHodler field (character)
-#' @return THe SystemMetadata (SystemMetadata)
-#' @export
-#'
-#' @examples
-#' create_sysmeta("my_pid", my_inv[1:10,], CN=Me,O=Test,C=US,DC=cilogon,DC=org")
-create_sysmeta <- function(pid, inventory_file, submitter, rights_holder) {
-  sysmeta <- new("SystemMetadata",
-                 identifier = pid,
-                 formatId = inventory_file[,"format_id"],
-                 size = inventory_file[,"size_bytes"],
-                 checksum = inventory_file[,"checksum_sha256"],
-                 checksumAlgorithm = "SHA256",
-                 submitter = submitter,
-                 rightsHolder = rights_holder,
-                 fileName = inventory_file[,"file"])
 
 
-  sysmeta <- datapackage::addAccessRule(sysmeta, "public", "read")
-  sysmeta <- datapackage::addAccessRule(sysmeta, submitter, "write")
-  sysmeta <- datapackage::addAccessRule(sysmeta, submitter, "changePermission")
 
-  sysmeta
-}
+# # DATA (MANY)
+# # Create them
+# # Mint PIDs, saving them for later
+# data_pids <- c()
+#
+# for (i in seq_len(nrow(files_data))) {
+#   path_on_disk <- paste0(base_path, files_data[i,"filename"])
+#
+#   # Generate and save PID
+#   if (!is.na(files_metadata[,"PID"])) {
+#     data_pid <- files_data[,"PID"]
+#   } else {
+#     data_pid <- dataone::generateIdentifier(mn)
+#   }
+#
+#   data_sysmeta <- NULL
+#   data_sysmeta <- tryCatch(
+#     {
+#       create_sysmeta(data_pid,
+#                      files_data[i,],
+#                      submitter,
+#                      rights_holder)
+#     },
+#     warning = function(w) {
+#       cat(paste0("Warning generated while creating SystemMetadata for ", files_data[i,"file"]))
+#     },
+#     error = function(e) {
+#       cat(paste0("Error generated while creating SystemMetadata for ", files_data[i,"file"]))
+#     })
+#
+#   if (is.null(data_sysmeta)) {
+#     cat(paste0("Failed to create SystemMetadata for a data object. Returning early.\n"))
+#     return(list("metadata_pid" = metadata_pid,
+#                 "metadata_created" = TRUE))
+#   }
+#
+#   cat(paste0("Caling MN.create() on ", files_data[i,"file"], " for PID '", data_pid, "'.\n"))
+#   data_create_result <- tryCatch(
+#     {
+#       dataone::create(mn,
+#                       data_pid,
+#                       file = path_on_disk,
+#                       sysmeta = data_sysmeta)
+#     },
+#     warning = function(w) {
+#       cat(paste0("Warning generated while calling MNStorage.create() for ", files_data[i,"file"]))
+#     },
+#     error = function(e) {
+#       cat(paste0("Error generated while calling MNStorage.create() for ", files_data[i,"file"]))
+#     })
+#
+#   # TODO check data_create_result
+#   data_create_result
+#
+#   if (is.null(data_create_result)) {
+#     cat(paste0("Failed to create data object. Returning early.\n"))
+#     return(list("metadata_pid" = metadata_pid,
+#                 "metadata_created" = TRUE))
+#   }
+#
+#   data_pids <- c(data_pids, data_pid)
+# }
+#
+# # Generate a resource map
+# # Create the resource map
+# # Create sysmeta for the resource map
+# resource_map_filepath <- create_resource_map(metadata_pid, data_pids, child_pids)
+# resource_map_size_bytes <- file.info(resource_map_filepath)$size
+# resource_map_checksum <- digest::digest(resource_map_filepath, algo = "sha256")
+# resource_map_pid <- paste0("resourceMap_", metadata_pid)
+#
+# resource_map_values <- data.frame("format_id" = "http://www.openarchives.org/ore/terms",
+#                                   "size_bytes" = resource_map_size_bytes,
+#                                   "checksum_sha256" = resource_map_checksum,
+#                                   "file" = paste0(resource_map_pid, ".xml"),
+#                                   stringsAsFactors = FALSE)
+#
+# resource_map_sysmeta <- create_sysmeta(resource_map_pid,
+#                                        resource_map_values[1,],
+#                                        me,
+#                                        rh)
+#
+# response <- tryCatch(
+#   {
+#     dataone::create(mn,
+#                     resource_map_pid,
+#                     file = resource_map_filepath,
+#                     sysmeta = resource_map_sysmeta)
+#   },
+#   warning = function(w) {
+#     cat(paste0("Warning generated while calling MNStorage.create() on Resource Map for package ", package, ".\n"))
+#   },
+#   error = function(e) {
+#     cat(paste0("Error generated while calling MNStorage.create() on Resource Map for package ", package, ".\n"))
+#   })
+#
+# # TODO Log response
+#
+# # Parse the response
+# stopifnot(!is.null(response))
+#
+# # Return the results of this call to the function
+# #
+# # We return all successfully created PIDs so we don't mint them again
+# # We return whether or not each metadata or data object was created
+# # successfully so we can know whether to insert it next time (if we have to
+# # run insert_package on the same package multiple times)
+#
+# list("metadata_pid" = metadata_pid,
+#      "metadata_created" = TRUE,
+#      "data_pids" = data_pids,
+#      "data_created" = rep(TRUE, length(data_pids)))
+# }
 
 
 #' Create a Resource Map. This is a convenience wrapper around the constructor
@@ -201,3 +261,163 @@ create_resource_map <- function(metadata_pid,
 
   outfilepath
 }
+
+get_or_create_pid <- function(file, mn, scheme="UUID") {
+  cat(paste0("get_or_create_pid()\n"))
+
+  stopifnot(is.data.frame(file),
+            nrow(file) == 1,
+            "pid" %in% names(file))
+
+  # Get the value of the PID
+  pid <- file[1,"pid"]
+
+  # Check if the existing PID is a valid one
+  if (is.character(pid) && nchar(pid) > 0) {
+    cat(paste0("Using alreayd-minted PID of ", pid, "\n"))
+    return(pid)
+  }
+
+  cat(paste0("Minting new PID...\n"))
+
+  # Try to generate a new pid with generateIdentifier()
+  pid <- NA
+
+  pid <- tryCatch(
+    {
+      dataone::generateIdentifier(mn, scheme)
+    },
+    error = function(e) {
+      # Do nothing
+    }
+  )
+
+  # Return `pid`, whch is either NA or a PID at this point
+  pid
+}
+
+
+
+
+create_sysmeta <- function(file, base_path, submitter, rights_holder) {
+  cat(paste0("create_sysmeta()\n"))
+
+  stopifnot(is.data.frame(file),
+            nrow(file) == 1)
+
+  stopifnot(is.character(base_path),
+            nchar(base_path) > 0)
+
+  path_on_disk <- paste0(base_path, file[1,"file"])
+  stopifnot(file.exists(path_on_disk))
+
+  # Get the PID
+  pid <- file[1,"pid"]
+  stopifnot(is.character(pid),
+            nchar(pid) > 0)
+
+  # Get other parameters for the System Metadata
+  format_id <- file[1,"format_id"]
+  size <- file[1,"size_bytes"]
+  checksum <- file[1,"checksum_sha256"]
+  checksum_algorithm <- "SHA256"
+  file_name <- file[1,"filename"]
+
+
+  sysmeta <- NULL
+  sysmeta <- tryCatch(
+    {
+      x <- new("SystemMetadata",
+               identifier = pid,
+               formatId = format_id,
+               size = size,
+               checksum = checksum,
+               checksumAlgorithm = checksum_algorithm,
+               submitter = submitter,
+               rightsHolder = rights_holder,
+               fileName = file_name)
+
+
+      x <- datapackage::addAccessRule(x, "public", "read")
+      x <- datapackage::addAccessRule(x, submitter, "write")
+      x <- datapackage::addAccessRule(x, submitter, "changePermission")
+
+      x
+    },
+    warning = function(w) {
+      cat(paste0("Warning generated during the call to create_sysmeta() for the metadata file ", file[1,"file"], "\n"))
+    },
+    error = function(e) {
+      cat(paste0("Error generated during the call to create_sysmeta() for the metadata file ", file[1,"file"], "\n"))
+      cat(as.character(e))
+    }
+  )
+
+  sysmeta
+}
+
+
+
+
+
+create_object <- function(file, sysmeta, base_path, mn) {
+  cat(paste0("create_object()\n"))
+
+  stopifnot(is.data.frame(file),
+            nrow(file) == 1,
+            "pid" %in% names(file),
+            "file" %in% names(file))
+
+  stopifnot(class(sysmeta) == "SystemMetadata")
+
+  stopifnot(is.character(base_path),
+            nchar(base_path) > 0)
+
+  stopifnot(class(mn) == "MNode")
+
+  # Set the return value to FALSE by default
+  result <- FALSE
+
+  # Get the PID
+  pid <- file[1,"pid"]
+  path_on_disk <- paste0(base_path, file[1,"file"])
+
+  # Run the create() call
+  response <- NULL
+  response <- tryCatch(
+    {
+      dataone::create(mn,
+                      pid,
+                      file = path_on_disk,
+                      sysmeta = sysmeta)
+    },
+    warning = function(w) {
+      cat(paste0("Warning generated during the call to MNStorage.create() for the metadata file ", file[1,"file"], "\n"))
+    },
+    error = function(e) {
+      cat(paste0("Error generated during the call to MNStorage.create() for the metadata file ", file[1,"file"], "\n"))
+      cat(as.character(e))
+    })
+
+  # Validate the result
+  # We use the XML package to convert the response to a list which just returns
+  # a string with the PID when we successfully created the object.
+
+  print(response)
+
+  created_pid <- XML::xmlToList(response)
+
+  if(is.character(created_pid) && nchar(created_pid) > 0) {
+    result <- TRUE
+  } else {
+    result <- FALSE
+  }
+
+  print(result)
+  result
+
+
+}
+
+
+
