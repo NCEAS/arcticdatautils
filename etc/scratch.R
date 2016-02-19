ffeb00c5b1e0a92be55dfacd9ef8b7a27ab9a65f
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-02-18 16:41:25 -0900

    Update scratch script

--- etc/scratch.R
+++ etc/scratch.R
@@ -8,3 +8,16 @@ inv <- inv_load_files("../planning/files.txt", inv)
 inv <- inv_load_sizes("../planning/sizes.txt", inv)
 inv <- inv_load_checksums("../planning/checksums.txt", inv)
 inv <- inv_add_extra_columns(inv)
+
+# Group into themes
+inv <- theme_packages(inv)
+
+
+packages <- inv[inv$is_metadata == TRUE,]
+table(packages$theme)
+
+many_files <- packages[packages$theme == "many-files",]
+
+table(many_files$subfolder)
+
+gateway_many <- many_files[many_files$subfolder == "G",]
