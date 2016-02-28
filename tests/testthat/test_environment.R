c63247b5ef252edb4df7f8bcded299c10fc0f36e
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-02-28 14:39:29 -0900

    Add test for env_load

--- tests/testthat/test_environment.R
+++ tests/testthat/test_environment.R
@@ -15,3 +15,13 @@ test_that("an environment string can be returned", {
   expect_is(env_get(), "character")
   expect_true(nchar(env_get()) > 0)
 })
+
+test_that("can correctly load the environment", {
+  Sys.setenv("ARCTICDATA_ENV" = "")
+
+  # Defaults to development if the env var isn't found
+  expect_true(env_get() == "development")
+
+  Sys.setenv("ARCTICDATA_ENV" = "production")
+  expect_true(env_get() == "production")
+})
