8c7d95fa3b5038e0390e4fa4f7b11e67a484f5fb
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-03-01 14:17:38 -0900

    Add basic util functions for interactively viewing packages

---
+++ R/interactive.R
@@ -0,0 +1,50 @@
+#' interactive.R
+#' Author: Bryce Mecum <mecum@nceas.ucsb.edu>
+#'
+#' Functions for interactive viewing of the Inventory and other objects.
+
+view_packages <- function(inventory) {
+  stopifnot(is.data.frame(inventory),
+            nrow(inventory) > 0)
+  stopifnot("package" %in% names(inventory))
+
+  packages <- unique(inventory$package)
+  viewing <- TRUE
+  pkg_index <- 0
+  while (viewing == TRUE) {
+    pkg_index <- pkg_index + 1
+    package <- packages[pkg_index]
+    show_package(inventory, package)
+
+    response <- wait_for_key()
+
+    if (response == "s") {
+      viewing <- FALSE
+    }
+  }
+}
+
+
+wait_for_key <- function() {
+  response <- readline(prompt = "Press [S]top or [C]ontinue")
+  response <- tolower(response)
+  response
+}
+
+
+show_package <- function(inventory, package) {
+  cat(paste0("Package: ", package, "\n"))
+
+  inv_p <- inventory[inventory$package == package,,drop = FALSE]
+  inv_p <- inv_p[!is.na(inv_p$file),]
+
+  metadata_files <- inv_p[inv_p$is_metadata == TRUE,]
+  data_files <- inv_p[inv_p$is_metadata == FALSE,]
+
+  cat(paste0("Metadata file: ", metadata_files[1,"filename"], "\n"))
+
+  cat(paste0("File in package: ", nrow(inv_p), "\n"))
+
+  cat(paste0("Data files...\n"))
+  print(data_files[,"filename"])
+}
