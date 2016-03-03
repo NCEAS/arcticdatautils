a3c0a4c9a32565e8c3d0b97f1a9a39fceb9628c0
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-03-02 15:18:24 -0900

    Update inventory tests to match new func signature

--- tests/testthat/test_inventory.R
+++ tests/testthat/test_inventory.R
@@ -9,24 +9,28 @@ test_that("an inventory can be created correctly", {
 test_that("an inventory can be populated with files", {
   # Case 1: Empty inv, non-empty file
   inv <- inv_init()
-  result <- inv_load_files(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "files_simple.txt"), inv)
+  result <- inv_load_files(inv,
+                           file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "files_simple.txt"))
   expect_true(nrow(result) == 3)
 
   # Case 2: Non-empty inv, non-empty file, where some are the same
   inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
-  result <- inv_load_files(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "files_simple.txt"), inv)
+  result <- inv_load_files(inv,
+                           file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "files_simple.txt"))
   expect_true(nrow(result) == 5)
 
   # Case 3: Non-empty inv, non-empty file, where none are the same
   inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
   inv <- subset(inv, file != "C")
-  result <- inv_load_files(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "files_simple.txt"), inv)
+  result <- inv_load_files(inv,
+                           file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "files_simple.txt"))
   expect_true(nrow(result) == 5)
 
   # Case 4: Non-empty inv, non-empty file, inventory already has columns
   inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
   inv$bytes <- rep(1000, nrow(inv))
-  result <- inv_load_files(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "files_simple.txt"), inv)
+  result <- inv_load_files(inv,
+                           file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "files_simple.txt"))
 
   expect_true(nrow(result) == 5)
   expect_true(ncol(result) == 2)
@@ -35,7 +39,8 @@ test_that("an inventory can be populated with files", {
 
 test_that("an inventory can be populated with byte sizes", {
   inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
-  result <- inv_load_sizes(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "sizes_simple.txt"), inv)
+  result <- inv_load_sizes(inv,
+                           file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "sizes_simple.txt"))
 
   expect_true(nrow(result) == 3)
   expect_true(ncol(result) == 2)
@@ -44,16 +49,20 @@ test_that("an inventory can be populated with byte sizes", {
 test_that("an inventory can be populated with checksums", {
   # Test if we can populate with checksums before sizes
   inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
-  inv_with_checksums <- inv_load_checksums(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"), inv)
-  inv_with_both <- inv_load_sizes(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "sizes_simple.txt"), inv_with_checksums)
+  inv_with_checksums <- inv_load_checksums(inv,
+                                           file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"))
+  inv_with_both <- inv_load_sizes(inv_with_checksums,
+                                  file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "sizes_simple.txt"))
 
   expect_true(nrow(inv_with_both) == 3)
   expect_true(ncol(inv_with_both) == 3)
 
   # Test if we can populate with sizes before checksums
   inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
-  inv_with_sizes <- inv_load_sizes(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "sizes_simple.txt"), inv)
-  inv_with_both <- inv_load_checksums(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"), inv_with_sizes)
+  inv_with_sizes <- inv_load_sizes(inv,
+                                   file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "sizes_simple.txt"))
+  inv_with_both <- inv_load_checksums(inv_with_sizes,
+                                      file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"))
 
   expect_true(nrow(inv_with_both) == 3)
   expect_true(ncol(inv_with_both) == 3)
@@ -62,9 +71,11 @@ test_that("an inventory can be populated with checksums", {
 # We should be able to call the same function multiple times and not break things
 test_that("calling things repeatedly does not break things", {
   inv <- read.csv(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "inventory_simple.csv"), stringsAsFactors = FALSE)
-  inv <- inv_load_checksums(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"), inv)
+  inv <- inv_load_checksums(inv,
+                            file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"))
   inv$test <- NA
-  inv <- inv_load_checksums(file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"), inv)
+  inv <- inv_load_checksums(inv,
+                            file.path(system.file("tests", "data", "inventory", package = "arcticdata"), "checksums_simple.txt"))
 
   expect_true(ncol(inv) == 3)
   expect_true(nrow(inv) == 3)
