ef10c62e5b913fc71a2d9938dd4f4af87379b9d0
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-03-07 15:41:59 -0900

    Add scripts for serial inserting

---
+++ etc/insert_all.R
@@ -0,0 +1,47 @@
+devtools::load_all(".")
+
+# Load inventory
+log_message("Loading inventory rda")
+load("data/inv.rda")
+inventory <- inv
+inventory$created <- FALSE
+inventory$ready <- TRUE
+
+# Set environment
+log_message("Setting environment to 'test'")
+Sys.setenv("ARCTICDATA_ENV" = "test")
+
+# Set token
+log_message("Setting the d1 token")
+token = Sys.getenv("D1TOKEN")
+stopifnot(nchar(token) > 0)
+options(authentication_token = token)
+
+# Mint PIDs for all objects
+log_message("Giving all objects new UUID pids")
+inventory$pid <- sapply(1:nrow(inventory), function(x) { paste0("urn:uuid:", uuid::UUIDgenerate()) })
+
+packages <- unique(inventory$package)
+
+for (package in packages) {
+  log_message(paste0("Inserting package ", package, "\n"))
+
+  insert_result <- tryCatch({
+    insert_package(inventory, package)
+  },
+  error = function(e) {
+    log_message(paste0("There was an error during the inserting of package ", package, "\n"))
+    log_message(e$message)
+    e
+  })
+
+  if (inherits(insert_result, "error")) {
+    next
+  }
+
+  log_message("Updating inventory...")
+  inventory <- inv_update(inventory, insert_result)
+
+  log_message("Writing inventory to disk...")
+  save(inventory, file = "inventory-latest.rda")
+}
---
+++ etc/insert_data.R
@@ -0,0 +1,48 @@
+devtools::load_all(".")
+
+# Load data
+log_message("Loading data rda")
+load("data/data.rda")
+inventory <- inv
+inventory$created <- FALSE
+inventory$ready <- TRUE
+
+# Set environment
+log_message("Setting environment to 'production'")
+Sys.setenv("ARCTICDATA_ENV" = "production")
+
+# Set token
+log_message("Setting the d1 token")
+token = Sys.getenv("D1TOKEN")
+stopifnot(nchar(token) > 0)
+options(authentication_token = token)
+
+# Mint PIDs for all objects
+log_message("Giving all objects new UUID pids")
+inventory$pid <- sapply(1:nrow(inventory), function(x) { paste0("urn:uuid:", uuid::UUIDgenerate()) })
+
+
+for (i in seq_len(nrow(inventory))) {
+  file_path <- inventory[i,"file"]
+
+  log_message(paste0("Inserting file ", file_path, "\n"))
+
+  insert_result <- tryCatch({
+    insert_file(inventory, file_path)
+  },
+  error = function(e) {
+    log_message(paste0("There was an error during the inserting of file ", file_path, "\n"))
+    log_message(e$message)
+    e
+  })
+
+  if (inherits(insert_result, "error")) {
+    next
+  }
+
+  log_message("Updating inventory...")
+  inventory <- inv_update(inventory, insert_result)
+
+  log_message("Writing inventory to disk...")
+  save(inventory, file = "inventory-latest.rda")
+}
