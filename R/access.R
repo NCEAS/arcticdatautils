aa77bd3245210fe8d068789e21a406e053607e17
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-04-27 16:52:08 -0800

    Move add_access_rules to access.R

--- R/access.R
+++ R/access.R
@@ -2,6 +2,38 @@
 #'
 #' Utility functions for getting and setting access rules for DataONE objects.
 
+
+#' Add access rules to the sysmeta object
+#'
+#' This is a function because I add a set of standard set of access rules to
+#' every object and the access rules don't differ across objects.
+#'
+#' @param sysmeta The SystemMetadata to add rules to (SystemMetadata)
+#'
+#' @return The modified SystemMetadata object
+#' @export
+#'
+#' @examples
+add_access_rules <- function(sysmeta) {
+  if (!inherits(sysmeta, "SystemMetadata")) {
+    log_message(paste0("An object of class ", class(sysmeta), " was passed in. Returning unmodified object.\n"))
+    return(sysmeta)
+  }
+
+  # Add myself explicitly as changePermission/write so I can update objects
+  # in the dev environment
+  if (env_get() == "development") {
+    sysmeta <- datapack::addAccessRule(sysmeta, env$submitter, "changePermission")
+  }
+
+  sysmeta <- datapack::addAccessRule(sysmeta, "public", "read")
+  sysmeta <- datapack::addAccessRule(sysmeta, "CN=arctic-data-admins,DC=dataone,DC=org", "write")
+  sysmeta <- datapack::addAccessRule(sysmeta, "CN=arctic-data-admins,DC=dataone,DC=org", "changePermission")
+
+  sysmeta
+}
+
+
 get_related_pids <- function(mn, pid) {
   stopifnot(is.character(mn),
             is.character(pid),
--- R/packaging.R
+++ R/packaging.R
@@ -567,38 +567,6 @@ create_sysmeta <- function(file, base_path, submitter, rights_holder) {
 }
 
 
-#' Add access rules to the sysmeta object
-#'
-#' This is a function because I add a set of standard set of access rules to
-#' every object and the access rules don't differ across objects.
-#'
-#' @param sysmeta The SystemMetadata to add rules to (SystemMetadata)
-#'
-#' @return The modified SystemMetadata object
-#' @export
-#'
-#' @examples
-add_access_rules <- function(sysmeta) {
-  if (!inherits(sysmeta, "SystemMetadata")) {
-    log_message(paste0("An object of class ", class(sysmeta), " was passed in. Returning unmodified object.\n"))
-    return(sysmeta)
-  }
-
-  # Add myself explicitly as changePermission/write so I can update objects
-  # in the dev environment
-  if (env_get() == "development") {
-    sysmeta <- datapack::addAccessRule(sysmeta, env$submitter, "changePermission")
-  }
-
-  sysmeta <- datapack::addAccessRule(sysmeta, "public", "read")
-  sysmeta <- datapack::addAccessRule(sysmeta, "CN=arctic-data-admins,DC=dataone,DC=org", "write")
-  sysmeta <- datapack::addAccessRule(sysmeta, "CN=arctic-data-admins,DC=dataone,DC=org", "changePermission")
-
-  sysmeta
-}
-
-
-
 #' Create an object from a row of the inventory.
 #'
 #' @param file A row from the inventory (data.frame)
