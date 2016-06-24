9b454aa4bf46af2212e54189c34d3e1384e42b72
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-06-24 15:21:52 -0800

    Add roxygen blocks to format functions

--- R/dataone_formats.R
+++ R/dataone_formats.R
@@ -6,5 +6,36 @@
 #' functions, a user's autocompletion routine in their editor can help them
 #' fill in the format ID they want.
 
-format_iso <- function() "http://www.isotc211.org/2005/gmd"
-format_eml <- function() "eml://ecoinformatics.org/eml-2.1.1"
+#' Helper function to generate the ISO 19139 format ID.w
+#'
+#' @return (character) The format ID for ISO 19139.
+#' @export
+#'
+#' @examples
+#' format_iso()
+#' \dontrun{
+#' # Upload a local ISO19139 XML file:
+#' env <- env_load()
+#' publish_object(env$mn, "path_to_some_EML_file", format_iso())
+#' }
+format_iso <- function() {
+  "http://www.isotc211.org/2005/gmd"
+}
+
+
+#' Helper function to generate the EML 2.1.1 format ID.
+#'
+#' @return (character) The format ID for EML 2.1.1.
+#' @export
+#'
+#' @examples
+#' format_eml
+#'
+#' \dontrun{
+#' # Upload a local EML 2.1.1 file:
+#' env <- env_load()
+#' publish_object(env$mn, "path_to_some_EML_file", format_eml())
+#' }
+format_eml <- function() {
+  "eml://ecoinformatics.org/eml-2.1.1"
+}
