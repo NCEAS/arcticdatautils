00e48e883ef579986db9a1b3f84672c174c5feba
Bryce Mecum <petridish@gmail.com>
2016-10-26 09:13:21 -0800

    Improve roxygen doc for format_id arg

--- R/editing.R
+++ R/editing.R
@@ -13,7 +13,7 @@
 #'
 #' @param mn (MNode) The Member Node to publish the object to.
 #' @param path the path to the file to be published
-#' @param format_id (character) Optional. The \href{https://cn.dataone.org/cn/v2/formats}{DataONE format ID} to set for the object.
+#' @param format_id (character) Optional. The format ID to set for the object. When not set, \code{\link{guess_format_id}} will be used to guess the format ID. Should be a \href{https://cn.dataone.org/cn/v2/formats}{DataONE format ID}.
 #' @param pid (character) Optional. The PID to use with the object.
 #' @param sid (character) Optional. The SID to use with the new object.
 #' @param clone_pid (character) PID of objet to clone System Metadata from
@@ -104,7 +104,7 @@ publish_object <- function(mn,
 #' @param mn (MNode) The Member Node to update the object on.
 #' @param pid (character) The PID of the object to update.
 #' @param path (character) The full path to the file to update with.
-#' @param format_id (character) Optional. Specify the format ID manually. Use this when `guess_format_id` wont' guess your format correctly. Should be a \href{https://cn.dataone.org/cn/v2/formats}{DataONE format ID}.
+#' @param format_id (character) Optional. The format ID to set for the object. When not set, \code{\link{guess_format_id}} will be used to guess the format ID. Should be a \href{https://cn.dataone.org/cn/v2/formats}{DataONE format ID}.
 #'
 #' @return (character) The PID of the updated object.
 #' @export
