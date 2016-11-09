246362068e2e546143125844ad41f7116006507f
Bryce Mecum <petridish@gmail.com>
2016-11-09 10:38:18 -0900

    Rename mn variable to node variable

--- tests/testthat/test_dataone.R
+++ tests/testthat/test_dataone.R
@@ -1,34 +1,34 @@
 context("dataone")
 
-mn <- env_load()$mn
+node <- env_load()$node
 
 test_that("permissions can be checked", {
-  if (!is_token_set(mn)) {
+  if (!is_token_set(node)) {
     skip("No token set. Skipping test.")
   }
 
-  id <- create_dummy_object(mn)
-  expect_true(is_authorized(mn, id, "write"))
+  id <- create_dummy_object(node)
+  expect_true(is_authorized(node, id, "write"))
 })
 
 test_that("permissions can be checked on multiple objects", {
-  if (!is_token_set(mn)) {
+  if (!is_token_set(node)) {
     skip("No token set. Skipping test.")
   }
 
-  a <- create_dummy_object(mn)
-  b <- create_dummy_object(mn)
+  a <- create_dummy_object(node)
+  b <- create_dummy_object(node)
 
-  expect_true(all(is_authorized(mn, c(a, b), "write")))
+  expect_true(all(is_authorized(node, c(a, b), "write")))
 })
 
 test_that("checking permissions on a missing object returns an error", {
-  if (!is_token_set(mn)) {
+  if (!is_token_set(node)) {
     skip("No token set. Skipping test.")
   }
 
   # Create a pid likely to not exist
   id <- paste0(uuid::UUIDgenerate(), uuid::UUIDgenerate())
 
-  expect_error(is_authorized(mn, id, "write"))
+  expect_error(is_authorized(node, id, "write"))
 })
