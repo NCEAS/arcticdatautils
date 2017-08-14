c685eb6e72e348edf20a4b920ef60946786a8feb
Bryce Mecum <petridish@gmail.com>
2017-08-14 11:20:00 -0800

    Add a test for resetting replication policies

--- tests/testthat/test_sysmeta.R
+++ tests/testthat/test_sysmeta.R
@@ -9,3 +9,16 @@ test_that("the replication policy gets cleared", {
   sysmeta <- clear_replication_policy(sysmeta)
   expect_false(sysmeta@replicationAllowed)
 })
+
+test_that("the replication policy gets defaulted correctly", {
+  # this is just a regression test
+
+  library(datapack)
+
+  sysmeta <- new("SystemMetadata")
+  sysmeta <- clear_replication_policy(sysmeta)
+
+  expect_false(sysmeta@replicationAllowed)
+  expect_equal(sysmeta@numberReplicas, 0)
+  expect_identical(sysmeta@blockedNodes, list("urn:node:KNB", "urn:node:mnUCSB1"))
+})
