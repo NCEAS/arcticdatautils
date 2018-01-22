5791dde723b3fc5262a94ba6724691176a422062
Bryce Mecum <petridish@gmail.com>
2018-01-22 01:27:00 -0900

    Remove test that relies on files in tests folder

--- tests/testthat/test_util.R
+++ tests/testthat/test_util.R
@@ -2,16 +2,6 @@
 
 context("util")
 
-test_that("identifiers can be extracted", {
-  x <- file.path(system.file("tests", "testfiles", package = "arcticdatautils"), "example-field-projects-file.xml")
-  identifier <- extract_local_identifier("field-projects", x)
-  expect_equal(identifier, "215.001")
-
-  x <- file.path(system.file("tests", "testfiles", package = "arcticdatautils"), "example-gateway-file.xml")
-  identifier <- extract_local_identifier("gateway", x)
-  expect_equal(identifier, "urn:x-wmo:md:org.aoncadis.www::d9330d2b-4174-11e3-8af4-00c0f03d5b7c")
-})
-
 test_that("paths can be joined", {
   expect_equal(path_join(""), "")
   expect_equal(path_join(1), "1")
