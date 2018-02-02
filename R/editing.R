fecd4827b62dacfd946bc5b167b34c232c03a9dd
Jeanette Clark <jclark@nceas.ucsb.edu>
2018-02-02 09:39:00 -0800

    added name and definition for param

--- R/editing.R
+++ R/editing.R
@@ -243,7 +243,7 @@ update_object <- function(mn, pid, path, format_id=NULL, new_pid=NULL, sid=NULL)
 #' This applies to the new metadata PID and its resource map and data object.
 #' access policies are not affected.
 #' @param check_first (logical) Optional. Whether to check the PIDs passed in as aruments exist on the MN before continuing. Checks that objects exist and are of the right format type. This speeds up the function, especially when `data_pids` has many elements.
-#' @param parent_data_pids
+#' @param parent_data_pids (character) Optional. Data pids of a parent package to be updated.
 #' @return pids (character) Named character vector of pids in the data package, including pids for the metadata, resource map, and data objects.
 #'
 #' @import dataone
