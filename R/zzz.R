8be1379a9aeeac1d1294770b5998e5316c7c0476
Bryce Mecum <petridish@gmail.com>
2018-02-06 14:44:20 -0900

    Remove warn_if_outdated as its no longer used

--- R/zzz.R
+++ R/zzz.R
@@ -25,26 +25,3 @@ load_d1_formats_list <- function(url) {
 
   D1_FORMATS <<- vapply(format_id_nodes, function(x) { xml2::xml_text(x) }, "")
 }
-
-warn_if_outdated <- function() {
-  installed_version <- utils::packageVersion("arcticdatautils")
-
-  req <- httr::GET("https://api.github.com/repos/nceas/arcticdatautils/releases/latest",
-                   httr::add_headers("Accept", "application/vnd.github.v3+json"))
-
-  release <- httr::content(req)
-
-  if (httr::status_code(req) != 200) {
-    warning(paste0("The request to check whether the version of arcticdatautils you have installed is the latest, the response from GitHub's API failed. The response was: \n\n", req))
-    return(invisible())
-  }
-
-  if (!("tag_name" %in% names(release))) {
-    warning(paste0("While checking to see if your installed version of arcticdatautils is the latest available, the response from GitHub's API was not as expected so checking was not done."))
-    return(invisible())
-  }
-
-  if (!stringr::str_detect(release$tag_name, paste0("^v", stringr::str_replace_all(as.character(installed_version), "\\.", "\\\\."), "$"))) {
-    warning(paste0("You do not have the most recent version of arcticdatautils installed. This is just a reminder to update next time you get the chance. Visit https://github.com/NCEAS/arcticdatautils/releases to find the latest release."))
-  }
-}
