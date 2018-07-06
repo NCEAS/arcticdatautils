04362602ee5809d5fb42500b4e4beb89813c522e
Dominic Mullen <dmullen17@gmail.com>
2018-07-06 13:10:23 -0700

    added mitchell's function

--- tests/testthat/test_eml.R
+++ tests/testthat/test_eml.R
@@ -184,3 +184,84 @@ test_that("eml_otherEntity_to_dataTable fails gracefully", {
   expect_equal(otherEntity@entityName, eml@dataset@dataTable[[1]]@entityName)
   expect_equivalent(otherEntity@physical, eml@dataset@dataTable[[1]]@physical)
 })
+
+test_that("which_in_eml Returns correct locations", {
+  if (!is_token_set(mn)) {
+    skip("No token set. Skipping test.")
+  }
+
+  attributes <-
+    data.frame(
+      attributeName = c(
+        "length_1",
+        "time_2",
+        "length_3"),
+      attributeDefinition = c(
+        "def 1",
+        "def 2",
+        "def 3"),
+      formatString = c(
+        NA,
+        NA,
+        NA),
+      measurementScale = c(
+        "ratio",
+        "ratio",
+        "ratio"),
+      domain = c(
+        "numericDomain",
+        "numericDomain",
+        "numericDomain"),
+      definition = c(
+        NA,
+        NA,
+        NA),
+      unit = c(
+        "meter",
+        "second",
+        "meter"),
+      numberType = c(
+        "real",
+        "real",
+        "real"),
+      stringsAsFactors = FALSE
+    )
+
+  attributeList <- EML::set_attributes(attributes)
+
+  dataTable_1 <- new("dataTable",
+                     entityName = "2016_data.csv",
+                     entityDescription = "2016 data",
+                     attributeList = attributeList)
+
+  dataTable_2 <- dataTable_1
+
+  dataTable_3 <- new("dataTable",
+                     entityName = "2015_data.csv",
+                     entityDescription = "2016 data",
+                     attributeList = attributeList)
+
+  creator_1 <- new("creator",
+                   individualName = new("individualName",
+                                        surName = "LAST",
+                                        givenName = "FIRST"))
+  creator_2 <- new("creator",
+                   individualName = new("individualName",
+                                        surName = "LAST",
+                                        givenName = "FIRST_2"))
+  creator_3 <- creator_2
+
+  title <- "Title"
+
+  dataset <- new("dataset",
+                 title = title,
+                 creator = c(creator_1, creator_2, creator_3),
+                 dataTable = c(dataTable_1, dataTable_2, dataTable_3))
+
+  eml <- new("eml",
+             dataset = dataset)
+
+  expect_equal(c(2,3), which_in_eml(eml@dataset@creator, "givenName", "FIRST_2"))
+  expect_error(which_in_eml(eml@dataset@dataTable, "attributeName", "length_3"))
+  expect_equal(c(1,3), which_in_eml(eml@dataset@dataTable[[1]]@attributeList@attribute, "attributeName", function(x) {grepl("^length", x)}))
+})
