bff4e477ae8b2eb29ae6f611a790c67a9d693bef
Derek Strong <dstrong@nceas.ucsb.edu>
2018-09-11 16:48:07 -0700

    Add update_package_object from datamgmt
    Close #96. Also move update_physical and create_dummy_package2 (renamed create_dummy_package_full) for dependency reasons.

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -17,3 +17,12 @@ test_that("a dummy package can be created", {
   expect_true(object_exists(mn, result$data))
   expect_true(object_exists(mn, result$resource_map))
 })
+
+test_that("create_dummy_package_full errors if wrong input", {
+  if (!is_token_set(mn)) {
+    skip("No token set. Skipping test.")
+  }
+
+  expect_error(create_dummy_package_full(mn, title = 11))
+  expect_error(create_dummy_package_full("mn"))
+})
