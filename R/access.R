58cb6222ef72917df2839a798c7e76f326dfa9ef
Jeanette Clark <jclark@nceas.ucsb.edu>
2019-09-20 09:59:46 -0700

    update other related functions with the same change

--- R/access.R
+++ R/access.R
@@ -91,6 +91,8 @@ set_rights_holder <- function(mn, pids, subject) {
 
   if (grepl("^https:\\/\\/orcid\\.org", subject)) {
     subject <- gsub("^https:\\/\\/orcid\\.org", "http:\\/\\/orcid\\.org", subject)
+    warning = function(w) {
+      message("Subject contains https, transforming to http")
   }
 
 
@@ -187,9 +189,11 @@ set_access <- function(mn, pids, subjects, permissions = c("read", "write", "cha
     stop("Argument 'subjects' must be character class with non-zero number of characters.")
   }
 
-  if (any(grepl("^https:\\/\\/orcid\\.org", subjects))) {
-    stop("Argument 'subjects' cannot contain 'https:', use 'http:' instead.")
-  }
+  if (grepl("^https:\\/\\/orcid\\.org", subject)) {
+    subject <- gsub("^https:\\/\\/orcid\\.org", "http:\\/\\/orcid\\.org", subject)
+    warning = function(w) {
+      message("Subject contains https, transforming to http")
+    }
 
   if (!all(permissions %in% c("read", "write", "changePermission"))) {
     stop("Argument 'permissions' must be one or more of: 'read', 'write', 'changePermission'")
@@ -377,8 +381,10 @@ set_rights_and_access <- function(mn, pids, subject, permissions = c("read", "wr
   }
 
   if (grepl("^https:\\/\\/orcid\\.org", subject)) {
-    stop("Argument 'subjects' cannot contain 'https:', use 'http:' instead.")
-  }
+    subject <- gsub("^https:\\/\\/orcid\\.org", "http:\\/\\/orcid\\.org", subject)
+    warning = function(w) {
+      message("Subject contains https, transforming to http")
+    }
 
   if (!all(permissions %in% c("read", "write", "changePermission"))) {
     stop("Argument 'permissions' must be one or more of: 'read', 'write', 'changePermission'")
