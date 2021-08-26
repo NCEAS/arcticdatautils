0a7583f84126802c4f9326d84f4839a9cf8bf033
laijasmine <jasminelai@nceas.ucsb.edu>
2021-08-26 12:12:27 -0700

    make parsing the mosaic owl file a separate function and reformat the code

--- R/mosaic.R
+++ R/mosaic.R
@@ -1,4 +1,5 @@
 
+
 #' Add a MOSAiC (https://mosaic-expedition.org/) attribute annotation (the returned object does not include the id slot)
 #'
 #' @param eventLabel (character) the event ID provided by the researcher
@@ -9,10 +10,7 @@
 #' @examples mosaic_annotate_attribute("PS122/2_14-270")
 mosaic_annotate_attribute <- function(eventLabel) {
   # get the owl file from github
-  mosaic_url <- "https://raw.githubusercontent.com/DataONEorg/sem-prov-ontologies/main/MOSAiC/MOSAiC.owl"
-  mosaic <- rdflib::rdf_parse(pins::pin(mosaic_url),
-    format = "rdfxml"
-  )
+  mosaic <- read_mosaic()
 
   # search for the event labels and corresponding devices (labels and URIs)
   query <-
@@ -35,29 +33,21 @@ mosaic_annotate_attribute <- function(eventLabel) {
   event_device_iri <- events %>%
     dplyr::filter(label == eventLabel)
 
-    #construct the annotation
+  #construct the annotation
   event_annotation <- list(
     # event
     list(
-      propertyURI = list(
-        label = "wasGeneratedBy",
-        propertyURI = "http://www.w3.org/ns/prov#wasGeneratedBy"
-      ),
-      valueURI = list(
-        label = event_device_iri$label[1],
-        valueURI = event_device_iri$event_iri[1]
-      )
+      propertyURI = list(label = "wasGeneratedBy",
+                         propertyURI = "http://www.w3.org/ns/prov#wasGeneratedBy"),
+      valueURI = list(label = event_device_iri$label[1],
+                      valueURI = event_device_iri$event_iri[1])
     ),
     # Method/Device
     list(
-      propertyURI = list(
-        label = "deployedSystem",
-        propertyURI = "https://purl.dataone.org/odo/MOSAIC_00002201"
-      ),
-      valueURI = list(
-        label = event_device_iri$dlabel[1],
-        valueURI = event_device_iri$device_iri[1]
-      )
+      propertyURI = list(label = "deployedSystem",
+                         propertyURI = "https://purl.dataone.org/odo/MOSAIC_00002201"),
+      valueURI = list(label = event_device_iri$dlabel[1],
+                      valueURI = event_device_iri$device_iri[1])
     )
   )
 
@@ -81,18 +71,14 @@ mosaic_annotate_attribute <- function(eventLabel) {
 #' #multiple campaigns
 #' mosaic_annotate_dataset(c("PS122/2", "PS122/1"))
 mosaic_annotate_dataset <- function(campaign) {
+  check_ps <-
+    purrr::map(campaign, ~ stringr::str_detect(.x, "PS", negate = T))
 
-  check_ps <- purrr::map(campaign, ~stringr::str_detect(.x, "PS", negate = T))
-
-  if(all(unlist(check_ps))){
+  if (all(unlist(check_ps))) {
     warning("Event id does not start with PS. Check if the basis is correct")
   }
 
-  # get the owl file from github
-  mosaic_url <- "https://raw.githubusercontent.com/DataONEorg/sem-prov-ontologies/main/MOSAiC/MOSAiC.owl"
-  mosaic <- rdflib::rdf_parse(pins::pin(mosaic_url),
-                              format = "rdfxml"
-  )
+  mosaic <- read_mosaic()
 
   #get the possible campaigns
   query <-
@@ -111,59 +97,41 @@ mosaic_annotate_dataset <- function(campaign) {
 
   campaign_iri <- dplyr::filter(df_campaign, label %in% campaign)
 
-  construct_campaign <- function(label, uri){
+  construct_campaign <- function(label, uri) {
     # Campaign
     list(
-      propertyURI = list(
-        label = "isPartOfCampaign",
-        propertyURI = "https://purl.dataone.org/odo/MOSAIC_00000032"
-      ),
-      valueURI = list(
-        label = label,
-        valueURI = uri
-      )
+      propertyURI = list(label = "isPartOfCampaign",
+                         propertyURI = "https://purl.dataone.org/odo/MOSAIC_00000032"),
+      valueURI = list(label = label,
+                      valueURI = uri)
     )
   }
 
-  campaigns <- purrr::map2(campaign_iri$label, campaign_iri$campaign_iri, construct_campaign)
+  campaigns <-
+    purrr::map2(campaign_iri$label,
+                campaign_iri$campaign_iri,
+                construct_campaign)
 
   #construct annotation
   standard_annotations <- list(
     # Basis
     list(
-      propertyURI = list(
-        label = "hasBasis",
-        propertyURI = "https://purl.dataone.org/odo/MOSAIC_00000034"
-      ),
-      valueURI = list(
-        label = "Polarstern",
-        valueURI = "https://purl.dataone.org/odo/MOSAIC_00000030"
-      )
+      propertyURI = list(label = "hasBasis",
+                         propertyURI = "https://purl.dataone.org/odo/MOSAIC_00000034"),
+      valueURI = list(label = "Polarstern",
+                      valueURI = "https://purl.dataone.org/odo/MOSAIC_00000030")
     ),
     # Project
     list(
-      propertyURI = list(
-        label = "hasProjectLabel",
-        propertyURI = "https://purl.dataone.org/odo/MOSAIC_00000025"
-      ),
-      valueURI = list(
-        label = "MOSAiC20192020",
-        valueURI = "https://purl.dataone.org/odo/MOSAIC_00000023"
-      )
+      propertyURI = list(label = "hasProjectLabel",
+                         propertyURI = "https://purl.dataone.org/odo/MOSAIC_00000025"),
+      valueURI = list(label = "MOSAiC20192020",
+                      valueURI = "https://purl.dataone.org/odo/MOSAIC_00000023")
     )
   )
 
   append(standard_annotations, campaigns)
 }
-
-library(rdflib)
-library(dplyr)
-
-# get the owl file from github
-mosaic_url <- "https://raw.githubusercontent.com/DataONEorg/sem-prov-ontologies/main/MOSAiC/MOSAiC.owl"
-mosaic <- rdflib::rdf_parse(pins::pin(mosaic_url),
-                            format = "rdfxml"
-)
 
 #' Creates the choice label pairs to be pasted into a portal document
 #'
@@ -176,8 +144,7 @@ mosaic <- rdflib::rdf_parse(pins::pin(mosaic_url),
 #' method <- mosaic_portal_filter("https://purl.dataone.org/odo/MOSAIC_00000036")
 #' basis <- mosaic_portal_filter("https://purl.dataone.org/odo/MOSAIC_00000028")
 #' campaign <- mosaic_portal_filter("https://purl.dataone.org/odo/MOSAIC_00000001")
-mosaic_portal_filter <- function(class){
-
+mosaic_portal_filter <- function(class) {
   #get the possible annotations under this class
 
   #build the SPARQL query
@@ -188,26 +155,36 @@ mosaic_portal_filter <- function(class){
 
    SELECT ?iri ?label
    WHERE {
-     ?iri rdf:type <", URI, "> .
+     ?iri rdf:type <",
+      URI,
+      "> .
      ?iri rdfs:label ?label .
    }"
     )
+
+  mosaic <- read_mosaic()
 
   df <- suppressMessages(rdflib::rdf_query(mosaic, query)) %>%
     arrange(label)
 
   #for method/devices find what the existing datasets are annotated with
-  if(class == "https://purl.dataone.org/odo/MOSAIC_00000036"){
+  if (class == "https://purl.dataone.org/odo/MOSAIC_00000036") {
     cn <- CNode('PROD')
     adc <- getMNode(cn, 'urn:node:ARCTIC')
 
     #get all the MOSAiC datasets
-    result <- query(adc, list(q = "sem_annotation:*MOSAIC* AND (*:* NOT obsoletedBy:*)",
-                              fl = "identifier,rightsHolder,formatId, fileName, dateUploaded, sem_annotation",
-                              sort = 'dateUploaded+desc',
-                              start ="0",
-                              rows = "1500"),
-                    as="data.frame")
+    result <-
+      query(
+        adc,
+        list(
+          q = "sem_annotation:*MOSAIC* AND (*:* NOT obsoletedBy:*)",
+          fl = "identifier,rightsHolder,formatId, fileName, dateUploaded, sem_annotation",
+          sort = 'dateUploaded+desc',
+          start = "0",
+          rows = "1500"
+        ),
+        as = "data.frame"
+      )
 
     #all the relevant annotations
     relevant <- unique(unlist(result$sem_annotation))
@@ -217,11 +194,33 @@ mosaic_portal_filter <- function(class){
 
   }
 
-  campaigns <- purrr::map2(df$label, df$iri,
-                           ~paste0("<choice><label>", .x, "</label><value>", .y, "</value></choice>"))
+  campaigns <- purrr::map2(
+    df$label,
+    df$iri,
+    ~ paste0(
+      "<choice><label>",
+      .x,
+      "</label><value>",
+      .y,
+      "</value></choice>"
+    )
+  )
 
   paste0(campaigns, collapse = "")
 
 }
 
-
+#helper
+#' Get the MOSAiC owl file from github
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_mosaic <- function() {
+  # get the owl file from github
+  mosaic_url <-
+    "https://raw.githubusercontent.com/DataONEorg/sem-prov-ontologies/main/MOSAiC/MOSAiC.owl"
+  mosaic <- rdflib::rdf_parse(pins::pin(mosaic_url),
+                              format = "rdfxml")
+}
